31c55150088d1575d3177fcdad9b54e165b44c9f
edvein-rin <edveinrin@gmail.com>
2021-08-09 13:38:10 +0300

    Open Weather Map API based on API interface

--- src/api/openweathermap.test.tsx
+++ src/api/openweathermap.test.tsx
@@ -1,20 +1,28 @@
 import process from 'process'
-import { getCoordsByCityName, Coords } from './openweathermap'
+
+import { Coords } from './api'
+import OpenWeatherMapAPI from './openweathermap'
 
 describe('API', () => {
-  test('.env access', async () => {
-    const apiKey = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY
+  const api = new OpenWeatherMapAPI(
+    process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY || ''
+  )
 
-    expect(typeof apiKey).toBe('string')
-  })
-
-  test('getCoordsByCityName', async () => {
+  test('getCoords', async () => {
     const coordsExpected: Coords = {
       lat: 51.5085,
       lon: -0.1257,
     }
 
-    const coords: Coords = await getCoordsByCityName('London')
+    const coords: Coords = await api.getCoords('London')
+
     expect(coords).toStrictEqual(coordsExpected)
+  })
+
+  test('getWeather', async () => {
+    await api.getWeather({
+      lat: 51.5085,
+      lon: -0.1257,
+    })
   })
 })
--- src/api/openweathermap.tsx
+++ src/api/openweathermap.tsx
@@ -1,38 +1,94 @@
-import process from 'process'
 import { Map } from 'immutable'
 
-const baseURL = 'http://api.openweathermap.org/'
-const APIKey = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY
+import {
+  request,
+  Options,
+  API,
+  Place,
+  City,
+  Coords,
+  Weather,
+  Forecast,
+} from './api'
 
-type RequestType = 'geo/1.0/direct' | 'data/2.5/weather' | 'data/2.5/onecall'
-type RequestOptions = Map<string, any>
+export type Service = 'geo/1.0/direct' | 'data/2.5/weather' | 'data/2.5/onecall'
 
-export interface Coords {
-  lat: number
-  lon: number
-}
+export default class OpenWeatherMapAPI implements API {
+  key: string
+  url: string = 'http://api.openweathermap.org/'
 
-export async function request(
-  type: RequestType,
-  options: RequestOptions
-): Promise<any> {
-  const link = new URL(baseURL + type)
-  const optionsWithAPIKey = options.set('appid', APIKey)
-  link.search = new URLSearchParams(optionsWithAPIKey.toObject()).toString()
-
-  const response = await fetch(link.toString())
-  const data = await response.json()
-
-  return data
-}
-
-export async function getCoordsByCityName(name: string): Promise<Coords> {
-  const response = await request('geo/1.0/direct', Map({ q: name }))
-  const city = response[0]
-  const coords: Coords = {
-    lat: city.lat,
-    lon: city.lon,
+  constructor(key: string) {
+    this.key = key
   }
 
-  return coords
+  call(service: Service, options: Options): Promise<Response> {
+    return request(this.url + service, options.set('appid', this.key))
+  }
+
+  async getCoords(city: City): Promise<Coords> {
+    const response = await this.call('geo/1.0/direct', Map({ q: city }))
+    const data = await response.json()
+
+    const firstCity = data[0]
+    const coords: Coords = {
+      lat: firstCity.lat,
+      lon: firstCity.lon,
+    }
+
+    return coords
+  }
+
+  async getWeather(place: Place): Promise<Weather> {
+    const isCity = typeof place === 'string'
+    const coords: Coords = isCity
+      ? await this.getCoords(place as string)
+      : (place as Coords)
+
+    const response = await this.call(
+      'data/2.5/onecall',
+      Map({ lat: coords.lat, lon: coords.lon, exclude: 'minutely,alerts' })
+    )
+    const data = await response.json()
+
+    const processForecast = (
+      forecast: any,
+      type: 'current' | 'hourly' | 'daily' = 'current'
+    ): Forecast => ({
+      time: forecast.dt,
+      temperature:
+        type !== 'daily'
+          ? forecast.temp
+          : {
+              morning: forecast.temp.morn,
+              day: forecast.temp.day,
+              evening: forecast.temp.eve,
+              night: forecast.temp.night,
+              min: forecast.temp.min,
+              max: forecast.temp.max,
+            },
+      feelsLike:
+        type !== 'daily'
+          ? forecast.feels_like
+          : {
+              morning: forecast.feels_like.morn,
+              day: forecast.feels_like.day,
+              evening: forecast.feels_like.eve,
+              night: forecast.feels_like.night,
+            },
+      windSpeed: forecast.wind_speed,
+      windDegree: forecast.wind_deg,
+      humidity: forecast.humidity,
+      shortDescription: forecast.weather[0].main,
+      fullDescription: forecast.weather[0].description,
+      icon: forecast.weather[0].icon,
+    })
+
+    const weather: Weather = {
+      current: processForecast(data.current),
+      hourly: data.hourly.map(processForecast, 'hourly'),
+      daily: data.daily.map(processForecast, 'daily'),
+    }
+
+    return weather
+  }
 }
