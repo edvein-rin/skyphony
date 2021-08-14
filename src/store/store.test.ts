0efd4f15a6cdd4ce5a3eaa625cf16839ad30da55
edvein-rin <edveinrin@gmail.com>
2021-08-14 13:03:46 +0300

    Store Thunk Fix

---
+++ src/store/store.test.ts
@@ -0,0 +1,41 @@
+import createStore from './store'
+import { LocationName, Location, City } from '../api/api'
+import { weatherSelector } from './selectors/weather'
+import updateWeather from './actions/updateWeather'
+import updateCity from './actions/updateCity'
+
+describe('Store', () => {
+  it('creates', () => {
+    const store = createStore()
+    expect(store).toBeDefined()
+  })
+
+  it('updates weather', async () => {
+    const store = createStore()
+    const location: Location = {
+      lat: 51.5085,
+      lon: -0.1257,
+    }
+
+    await store.dispatch(updateWeather(location))
+
+    expect(weatherSelector(store.getState())).toBeDefined()
+  })
+
+  it.only('updates city', async () => {
+    const cityExpected: City = {
+      coords: {
+        lat: 51.5085,
+        lon: -0.1257,
+      },
+      name: 'London',
+    }
+
+    const store = createStore()
+    const locationName: LocationName = 'London'
+
+    await store.dispatch(updateCity(locationName))
+
+    expect(store.getState().city).toStrictEqual(cityExpected)
+  })
+})
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,21 @@
-import { Action, applyMiddleware, createStore, Store } from 'redux'
-import thunk, { ThunkMiddleware } from 'redux-thunk'
+import {
+  applyMiddleware,
+  createStore as createReduxStore,
+  Store as ReduxStore,
+} from 'redux'
+import thunk, { ThunkDispatch, ThunkMiddleware } from 'redux-thunk'
 
 import rootReducer from './reducers/root'
 import State from './state'
+import Action from './action'
 
-const store: Store<State> = createStore(
-  rootReducer,
-  applyMiddleware(thunk as ThunkMiddleware<State, Action>)
-)
+export type Store = ReduxStore<State, Action> & {
+  dispatch: ThunkDispatch<State, undefined, Action>
+}
 
-export default store
+export default function createStore(): Store {
+  return createReduxStore(
+    rootReducer,
+    applyMiddleware(thunk as ThunkMiddleware<State, Action>)
+  )
+}
