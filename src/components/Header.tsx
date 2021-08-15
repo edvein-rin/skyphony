57fcbed012016d27610998e00b31db7c387e0581
edvein-rin <edveinrin@gmail.com>
2021-08-15 16:36:30 +0300

    Search UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components/macro'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
+import InputBase from '@material-ui/core/InputBase'
+import SearchIconMD from '@material-ui/icons/Search'
 
 import logo from '../assets/logo.svg'
 
@@ -9,10 +11,44 @@ export default function Header() {
       <Toolbar>
         <Logo alt='Skyphony' src={logo} height={20} />
         <Title variant='h6'>Skyphony</Title>
+        <Search>
+          <SearchInput
+            placeholder='Type a city name...'
+            inputProps={{ 'aria-label': 'search' }}
+          />
+          <IconButton>
+            <SearchIcon fontSize='small' />
+          </IconButton>
+        </Search>
       </Toolbar>
     </AppBar>
   )
 }
+
+export const Search = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  max-width: 250px;
+
+  padding: 0 ${({ theme }) => theme.spacing(1)}px;
+
+  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+  background-color: ${({ theme }) => theme.palette.secondary.main};
+  color: ${({ theme }) => theme.palette.secondary.constrastText};
+`
+
+export const SearchIcon = styled(SearchIconMD)`
+  color: ${({ theme }) => theme.palette.secondary.contrastText};
+`
+
+export const SearchInput = styled(InputBase)`
+  color: ${({ theme }) => theme.palette.secondary.contrastText};
+  margin-right: ${({ theme }) => theme.spacing(1)}px;
+  flex-grow: 1;
+`
 
 export const Logo = styled.img`
   margin-right: ${(props) => props.theme.spacing(2)}px;
